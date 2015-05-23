bcc35ff60eb065bfd71152719a3be504b3cb4d45
Scott Harris <scottharris@outlook.com>
2015-05-23 16:44:28 -0400

    initial commit

---
+++ MemeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemeGame
+//
+//  Created by scott harris on 5/23/15.
+//  Copyright (c) 2015 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemeGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemeGame
+//
+//  Created by scott harris on 5/23/15.
+//  Copyright (c) 2015 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
